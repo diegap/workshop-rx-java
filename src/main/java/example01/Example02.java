f57e39cf58b8f69eed874b8405c451d5d6697be8
Fabio Negri <fabionegri1990@gmail.com>
2019-10-15 23:57:03 -0300

    Subo Example02.java

---
+++ src/main/java/example01/Example02.java
@@ -0,0 +1,21 @@
+package example01;
+
+import io.reactivex.Maybe;
+import io.reactivex.Single;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Example02 {
+
+    public Single<String> singleElement() {
+        throw new NotImplementedException();
+    }
+
+    public Maybe<String> maybeElement() {
+        throw new NotImplementedException();
+    }
+
+    public Maybe<String> completableElement() {
+        throw new NotImplementedException();
+    }
+
+}
